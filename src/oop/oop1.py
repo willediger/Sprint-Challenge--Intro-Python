24d50742dd1330c34fd7313daf05094c5ff11a8a
Will Ediger <will.ediger@gmail.com>
2020-05-15 21:06:01 -0500

    oop1 completed, passing tests

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -7,6 +7,29 @@
 #   |       |
 #   v       v
 # [Car]  [Motorcycle]
+class Vehicle:
+    #base class
+    pass
+
+class FlightVehicle(Vehicle):
+    pass
+
+class Starship(FlightVehicle):
+    pass
+
+class Airplane(FlightVehicle):
+    pass
+
+class GroundVehicle(Vehicle):
+    pass
+
+class Car(GroundVehicle):
+    pass
+
+class Motorcycle(GroundVehicle):
+    pass
+
+
 #
 # Each class can simply "pass" for its body. The exercise is about setting up
 # the hierarchy.
